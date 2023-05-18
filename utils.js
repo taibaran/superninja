7f7160bc0900d8dd252c6699ee72c195f3d16023
Tai Baran <tai.baran3@gmail.com>
2023-05-18 14:23:26 +0300

    upd1

---
+++ utils.js
@@ -0,0 +1,8 @@
+﻿function writeToDiv(divId, htmlStr, override) {
+    let d = document.getElementById(divId);
+    if (override)
+        d.innerHTML = htmlStr;
+    else
+        d.innerHTML += htmlStr;
+
+}
